52d843c44b932ca3668fb9ae14954c6f19e7412f
me <me@maincomp>
2017-10-04 17:37:49 -0700

    Added library functions (print_hex, print_string), first stage BL now loads and transfers control to second stage

--- bootsec.asm
+++ bootsec.asm
@@ -1,20 +1,50 @@
-mov ah, 0x0e   ;set tty mode
-mov al, 'H'
-int 0x10
+;                Setup
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-mov al, 'e'
-int 0x10
+[org 0x7c00] ;BIOS loads this program to 0x7c00
+             ;all variables are referenced 
+             ;relative to this point
+;Set bios to output to tty
+mov ah, 0x0e
 
-mov al, 'l'
-int 0x10
+;Initilize stack
+mov bp, 0x8000
+mov sp, bp
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-mov al, 'l'
-int 0x10
+firstStageBoot:
+   ;Load sectors from disk 
+   mov byte [boot_drive], dl
+   
+   mov al, 1                   ;number of sectors to read
+   mov dl, [boot_drive]        ;drive to read from
+   mov bx, secondStageBoot     ;place read sectors here
+   mov ah, 0x02                ;bios disk read mode
+   mov ch, 0                   ;cylinder number
+   mov dh, 0                   ;head number
+   mov cl, 2                   ;starting sector number
+   int 0x13                    ;generate interrupt
 
-mov al, 'o'
-int 0x10
+   jmp secondStageBoot
 
-jmp $ ;
+;sysSuspend:
+;   cli 
+;   hlt
+;   jmp sysSuspend
 
-times 510 - ($-$$) db 0
+%include "lib.asm"
+boot_drive db 0
+
+;             Padding and magic bit
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+times 510-($-$$) db 0
 dw 0xaa55
+
+
+;Everything below is loaded by the first stage
+secondStageBoot:
+   mov bx, myMsg
+   call print_string
+   jmp $
+
+myMsg db "Successfully jumped!", 0
---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nasm ${1}.asm -f bin -o ${1}.bin 
